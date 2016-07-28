b573ab82635055a233b3473a11fa48f7766c0b4d
Marco Martin <notmart@gmail.com>
2016-07-28 17:58:30 +0200

    try to use internal Qt translations system

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/libkirigamiplugin.pot
+$EXTRACT_TR_STRINGS `find . -name \*.qml` -L Java -o $podir/libkirigamiplugin_qt.pot
 rm -f rc.cpp
 
