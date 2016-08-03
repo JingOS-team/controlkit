c498a8c723165d62921839936dcbad4e163cddd1
Luigi Toscano <luigi.toscano@tiscali.it>
2016-08-04 01:38:16 +0200

    Fix message extraction

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACT_TR_STRINGS `find . -name \*.qml` -L Java -o $podir/libkirigamiplugin_qt.pot
+$EXTRACT_TR_STRINGS `find . -name \*.qml` -o $podir/libkirigamiplugin_qt.pot
 rm -f rc.cpp
 
