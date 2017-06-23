a8bb82bebf084cb957048f5b77081bf275dc2250
Marco Martin <notmart@gmail.com>
2017-06-23 13:10:06 +0200

    color the status bar in Android
    use qtandroidextras and call the appropriate Java methods

--- examples/android/main.cpp
+++ examples/android/main.cpp
@@ -4,6 +4,19 @@
 #include <QQmlApplicationEngine>
 #include <QtQml>
 #include <QUrl>
+#include <QColor>
+
+#ifdef Q_OS_ANDROID
+#include <QtAndroid>
+
+// WindowManager.LayoutParams
+#define FLAG_TRANSLUCENT_STATUS 0x04000000
+#define FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS 0x80000000
+// View
+#define SYSTEM_UI_FLAG_LIGHT_STATUS_BAR 0x00002000
+
+#endif
+
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
@@ -19,6 +32,17 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     if (engine.rootObjects().isEmpty()) {
         return -1;
     }
+
+    //HACK to color the system bar on Android, use qtandroidextras and call the appropriate Java methods 
+#ifdef Q_OS_ANDROID
+    QtAndroid::runOnAndroidThread([=]() {
+        QAndroidJniObject window = QtAndroid::androidActivity().callObjectMethod("getWindow", "()Landroid/view/Window;");
+        window.callMethod<void>("addFlags", "(I)V", FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.callMethod<void>("clearFlags", "(I)V", FLAG_TRANSLUCENT_STATUS);
+        window.callMethod<void>("setStatusBarColor", "(I)V", QColor("#2196f3").rgba());
+        window.callMethod<void>("setNavigationBarColor", "(I)V", QColor("#2196f3").rgba());
+    });
+#endif
 
     return app.exec();
 }
