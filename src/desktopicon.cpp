JingOS-team/controlkit

commit db6ef33cfa9b277f6c88679a57b50fb2afccff89

Allow qrc assets to be used in Actions icons
Note that this isn't 100% correct. Imageprovider strings are not
really URL, they are URI, it can be anything and include URI
arguments.

Reviewers: mart

Reviewed By: mart

Subscribers: plasma-devel

Tags: #kirigami

Differential Revision: https://phabricator.kde.org/D13054

src/desktopicon.cpp
@@ -435,6 +435,12 @@ QImage DesktopIcon::findIcon(const QSize &size)
         QUrl iconUrl(iconSource);
         QString iconProviderId = iconUrl.host();
         QString iconId = iconUrl.path();
+
+        // QRC paths are not correctly handled by .path()
+        if (iconId.size() >=2 && iconId.startWith(QLatin1String("/:"))) {
+            iconId = iconId.remove(0, 1);
+        }
+
         QSize actualSize;
         QQuickImageProvider* imageProvider = dynamic_cast<QQuickImageProvider*>(
                     qmlEngine(this)->imageProvider(iconProviderId));

src/controls/templates/SwipeListItem.qml
@@ -238,7 +238,7 @@ T2.ItemDelegate {
                 delegate: Icon {
                     height: actionsLayout.height
                     width: height
-                    source: modelData.iconName
+                    source: modelData.iconName != "" ? modelData.iconName : modelData.iconSource
                     enabled: (modelData && modelData.enabled !== undefined) ? modelData.enabled : true;
                     visible: (modelData && modelData.visible !== undefined) ? modelData.visible : true;
                     MouseArea {

commit 89932f89051777a649fe5997fc25811abcdbb3e1

build: Fix a regression from db6ef33cfa9
Sorry, that's on me. I copy/pasted proposed changes without waiting
for the CI. Sorry for breaking the build

src/desktopicon.cpp
@@ -437,7 +437,7 @@ QImage DesktopIcon::findIcon(const QSize &size)
         QString iconId = iconUrl.path();
 
         // QRC paths are not correctly handled by .path()
-        if (iconId.size() >=2 && iconId.startWith(QLatin1String("/:"))) {
+        if (iconId.size() >=2 && iconId.startsWith(QLatin1String("/:"))) {
             iconId = iconId.remove(0, 1);
         }
 