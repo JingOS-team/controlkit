3f55e64993f9683b49732680ad33e38d89fad751
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-02-08 17:24:47 +0100

    Relicense to LGPL-2.0-or-later
    Summary:
    Request to Marco to relicense both files to the license
    LGPL-2.0-or-later, which is the default license for all other
    files in this repository. Right now, the these files are licensed
    via GPL and thus making the compiled Kirigami library a GPL-library.
    
    Reviewers: mart
    
    Reviewed By: mart
    
    Subscribers: plasma-devel
    
    Tags: #kirigami
    
    Differential Revision: https://phabricator.kde.org/D27251

--- src/libkirigami/tabletmodewatcher.cpp
+++ src/libkirigami/tabletmodewatcher.cpp
@@ -1,23 +1,21 @@
 /*
- * Copyright 2018 Marco Martin <mart@kde.org>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+*   Copyright 2018 Marco Martin <mart@kde.org>
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU Library General Public License as
+*   published by the Free Software Foundation; either version 2, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Library General Public License for more details
+*
+*   You should have received a copy of the GNU Library General Public
+*   License along with this program; if not, write to the
+*   Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "tabletmodewatcher.h"
 
@@ -52,7 +50,7 @@ public:
 #elif defined(KIRIGAMI_ENABLE_DBUS)
         //Mostly for debug purposes and for platforms which are always mobile,
         //such as Plasma Mobile
-        if (qEnvironmentVariableIsSet("QT_QUICK_CONTROLS_MOBILE") ||        
+        if (qEnvironmentVariableIsSet("QT_QUICK_CONTROLS_MOBILE") ||
             qEnvironmentVariableIsSet("KDE_KIRIGAMI_TABLET_MODE")) {
             isTabletMode = (QString::fromLatin1(qgetenv("QT_QUICK_CONTROLS_MOBILE")) == QStringLiteral("1") ||
                 QString::fromLatin1(qgetenv("QT_QUICK_CONTROLS_MOBILE")) == QStringLiteral("true")) ||
