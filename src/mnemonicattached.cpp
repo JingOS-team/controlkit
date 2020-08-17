6dda5f1d3782b2c8aeb385fdade6e9487c29f571
Marco Martin <notmart@gmail.com>
2020-08-17 11:19:17 +0200

    new logic to remove acceleration marks
    Logic to remove acceleration marks borrowed from KWidgetAddons
    https://phabricator.kde.org/D7964
    This should handle as wel CJK text.
    BUG:420409

--- src/mnemonicattached.cpp
+++ src/mnemonicattached.cpp
@@ -12,6 +12,92 @@
 
 QHash<QKeySequence, MnemonicAttached *> MnemonicAttached::s_sequenceToObject = QHash<QKeySequence, MnemonicAttached *>();
 
+// If pos points to alphanumeric X in "...(X)...", which is preceded or
+// followed only by non-alphanumerics, then "(X)" gets removed.
+static QString removeReducedCJKAccMark(const QString &label, int pos)
+{
+    if (pos > 0 && pos + 1 < label.length()
+            && label[pos - 1] == QLatin1Char('(') && label[pos + 1] == QLatin1Char(')')
+            && label[pos].isLetterOrNumber()) {
+        // Check if at start or end, ignoring non-alphanumerics.
+        int len = label.length();
+        int p1 = pos - 2;
+        while (p1 >= 0 && !label[p1].isLetterOrNumber()) {
+            --p1;
+        }
+        ++p1;
+        int p2 = pos + 2;
+        while (p2 < len && !label[p2].isLetterOrNumber()) {
+            ++p2;
+        }
+        --p2;
+
+        if (p1 == 0) {
+            return label.leftRef(pos - 1) + label.midRef(p2 + 1);
+        } else if (p2 + 1 == len) {
+            return label.leftRef(p1) + label.midRef(pos + 2);
+        }
+    }
+    return label;
+}
+
+QString removeAcceleratorMarker(const QString &label_)
+{
+    QString label = label_;
+
+    int p = 0;
+    bool accmarkRemoved = false;
+    while (true) {
+        p = label.indexOf(QLatin1Char('&'), p);
+        if (p < 0 || p + 1 == label.length()) {
+            break;
+        }
+
+        if (label[p + 1].isLetterOrNumber()) {
+            // Valid accelerator.
+            label = QString(label.leftRef(p) + label.midRef(p + 1));
+
+            // May have been an accelerator in CJK-style "(&X)"
+            // at the start or end of text.
+            label = removeReducedCJKAccMark(label, p);
+
+            accmarkRemoved = true;
+        } else if (label[p + 1] == QLatin1Char('&')) {
+            // Escaped accelerator marker.
+            label = QString(label.leftRef(p) + label.midRef(p + 1));
+        }
+
+        ++p;
+    }
+
+    // If no marker was removed, and there are CJK characters in the label,
+    // also try to remove reduced CJK marker -- something may have removed
+    // ampersand beforehand.
+    if (!accmarkRemoved) {
+        bool hasCJK = false;
+        for (const QChar c : qAsConst(label)) {
+            if (c.unicode() >= 0x2e00) { // rough, but should be sufficient
+                hasCJK = true;
+                break;
+            }
+        }
+        if (hasCJK) {
+            p = 0;
+            while (true) {
+                p = label.indexOf(QLatin1Char('('), p);
+                if (p < 0) {
+                    break;
+                }
+                label = removeReducedCJKAccMark(label, p + 1);
+                ++p;
+            }
+        }
+    }
+
+    return label;
+}
+
+
 MnemonicAttached::MnemonicAttached(QObject *parent)
     : QObject(parent)
 {
@@ -71,7 +157,7 @@ bool MnemonicAttached::eventFilter(QObject *watched, QEvent *e)
         QKeyEvent *ke = static_cast<QKeyEvent *>(e);
         if (ke->key() == Qt::Key_Alt) {
             m_actualRichTextLabel = m_label;
-            m_actualRichTextLabel.replace(QRegularExpression(QStringLiteral("\\&([^\\&])")), QStringLiteral("\\1"));
+            m_actualRichTextLabel = removeAcceleratorMarker(m_actualRichTextLabel);
             emit richTextLabelChanged();
             m_active = false;
             emit activeChanged();
@@ -164,7 +250,7 @@ void MnemonicAttached::updateSequence()
 
     if (!m_enabled) {
         m_actualRichTextLabel = text;
-        m_actualRichTextLabel.replace(QRegularExpression(QStringLiteral("\\&([^\\&])")), QStringLiteral("\\1"));
+        m_actualRichTextLabel = removeAcceleratorMarker(m_actualRichTextLabel);
         //was the label already completely plain text? try to limit signal emission
         if (m_mnemonicLabel != m_actualRichTextLabel) {
             m_mnemonicLabel = m_actualRichTextLabel;
@@ -221,7 +307,7 @@ void MnemonicAttached::updateSequence()
         emit sequenceChanged();
     } else {
         m_actualRichTextLabel = text;
-        m_actualRichTextLabel.replace(QRegularExpression(QStringLiteral("\\&([^\\&])")), QStringLiteral("\\1"));
+        m_actualRichTextLabel = removeAcceleratorMarker(m_actualRichTextLabel);
         m_mnemonicLabel = m_actualRichTextLabel;
         
     }
